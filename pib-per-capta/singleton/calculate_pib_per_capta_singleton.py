96d6fe94eeaee33c380cc88046e96fdceef2dcd7
paulademelo <56200258+paulademelo@users.noreply.github.com>
2022-10-07 22:45:26 -0300

    Atividade

--- pib-per-capta/singleton/calculate_pib_per_capta_singleton.py
+++ pib-per-capta/singleton/calculate_pib_per_capta_singleton.py
@@ -1,4 +1,5 @@
 import abc
+from itertools import count
 from json import load
 from os import stat
 from typing_extensions import Self
@@ -8,25 +9,6 @@ from abc import ABC, abstractclassmethod, abstractmethod
 class Config:
     _base_file  = "data/base.xlsx"
     _region     = "UF_Regiao"
-
-'''
-Uma classe abstrata representa um contrato com regras,
-cada regra em um código é uma função ou método
-que contém as assinaturas.
-Uma assinatura é a definição de um método, com parâmetros,
-tipos e retorno.
-
-No exemplo abaixo CalculatePibPerCapta:
-1 - Quantos contratos temos? 
-R: 1
-2 - Quantas regras temos?
-R: 4
-3 - Quantas assinaturas temos?
-R: 4
-4- Como podemos violar o contrado da AbstractCalculatePib?
-R: Não implementando a classe AbstractCalculatePib ou não implementando ao menos um método da classe CalculatePibPerCaptaSingleton
-'''
-
 
 class AbstractCalculatePib(ABC):
     class AbstractCalculatePib(ABC):
@@ -53,6 +35,10 @@ class AbstractCalculatePib(ABC):
         @abstractclassmethod
         def get_region_by_state(self):
             raise RuntimeError('TODO: Método ainda não implementado')
+
+        @abstractclassmethod
+        def get_state_by_region_for(self):
+            raise RuntimeError('TODO: Método ainda não implementado')    
   
 class CalculatePibPerCaptaSingleton(AbstractCalculatePib):
     # Atributos da classe
@@ -76,12 +62,6 @@ class CalculatePibPerCaptaSingleton(AbstractCalculatePib):
         else:
             print("Instance has already been created")          
             return cls._instance
-
-        # 1 - Criar uma instancia unica
-        # sugestao de implementacao:
-        # https://python-patterns.guide/gang-of-four/singleton/
-        # https://github.com/Sean-Bradley/Design-Patterns-In-Python/tree/master/singleton
-
     
     @classmethod
     def load_file(self):
@@ -116,10 +96,88 @@ class CalculatePibPerCaptaSingleton(AbstractCalculatePib):
     @classmethod
     def get_region_by_state(self, region):
 
-        print("------- Atividade 1 -------")
+        print("------- Atividade 2 -------")
         regions = self.current_content[self.current_content['Estado'] == region]
         print(+ regions + " |" )
 
+    '''
+    Atividade 4)
+    Retorna o estado de acordo com a região usando for
+    '''
+    @classmethod
+    def get_state_by_region_for(self, state):
+        print("------- Atividade 4.1 usando For: -------")
+        list = []
+        for item in self.current_content[self.current_content['Regiao'] == state]:
+            if state in item:
+                list.append(item)
+            return list
+
+    ''' 
+    Retorna o estado de acordo com a região usando iteração com pandas Dataframe
+    '''
+    @classmethod
+    def get_state_by_region_data_frame(self, state):
+       print("------- Atividade 4.2 usando Dataframe -------")
+       for i in self.current_content:
+        return pd.DataFrame(self.current_content, columns=['Estado', 'Regiao'])
+       
+
+    '''
+    Atividade 5) Criar os seguintes métodos para sumarizar o resultado de Região e Estado:
+    5.1
+    Retorna o total de estados
+    '''
+    @classmethod
+    def state_count(self):
+        print("------- Atividade 5.1 Total de Estados -------")
+        print(len(self.current_content))  
+
+    '''
+    Atividade 5)
+    5.2
+    Retorna o total de regiões
+    '''
+    @classmethod
+    def region_count(self):
+        print("------- Atividade 5.2 Total de Regiões -------")
+        for i in self.current_content:
+            if i in self.current_content[self.current_content['Regiao'] == i]:
+                regions = i
+        print(len(regions))
+    
+    ''' 5.3 
+    Retorna a primeira região e estado em uma lista
+    '''
+    @classmethod
+    def first_region_state(self):
+        print("------- Atividade 5.3 primeira região e estado-------")
+        first_region = pd.DataFrame(self.current_content, columns=['Estado', 'Regiao'])
+        print(first_region.iloc[0,0]+ " -> " + first_region.iloc[0,1])
+   
+    ''' 5.4 
+    Retorna a última região e estado em uma lista
+    '''
+    @classmethod
+    def last_region_state(self):
+        print("------- Atividade 5.3 última região e estado-------")
+        last_region = pd.DataFrame(self.current_content, columns=['Estado', 'Regiao'])
+        print(last_region.iloc[-1,0] + " -> " + last_region.iloc[-1,-1])
+   
+    '''
+    Atividade 3)
+    3.1 Qual a finalidade do Pacote Pandas do Python?
+        R: O Pandas é uma biblioteca para manipulação dos dados
+
+    3.2 Como o Pandas é organizado internamente em termos de estrutura de Dados em Python?
+        R: Existem dois tipos de estruturas: pseires (unidimensional, o seja, representa um lista, e o DataFrame,
+        que são abstrações de uma tabela.
+
+    3.3 Qual a diferença entre um comando de iteração classico em Python e o uso de filtros customizados no Panda?
+        R: A diferença está na rapidez, o filter consegue ser mais eficiente
+    '''
+
+                       
 
 
 
